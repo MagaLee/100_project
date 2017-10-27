6dee6b3d9beb56e1c4aeef892940383062caeadc
Maga Lee <irenestory06@gmail.com>
2017-10-27 13:33:15 +0900

    init

---
+++ index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title> Instagram </title>
+
+	<!-- starts google font-->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+	<!-- ends google -->
+
+	<!-- staerts an external style sheet -->
+	<link rel="stylesheet" href="css/02_main.css">
+	<link rel="stylesheet" href="css/00_overlrap.css">
+	<!-- ends an external style sheet -->
+
+	<style>
+		main{
+			width: 1200px;
+		}
+	</style>
+
+</head>
+
+<body>
+	<!-- starts a navigation -->
+	<nav>
+		<header>
+			<div class="navWrapper">
+
+				<!-- starts insta logos -->
+				<div class="group_01">
+					<img class="picLogo" src="images/sns_icons/instagram-glyph.svg" />
+					<span class="borderRight"></span>
+					<img class="nameLogo" src="images/sns_icons/instagram.svg" />
+				</div>
+				<!-- ends insta logos -->
+
+
+				<!-- starts a search -->
+				<input class="search" type="text" placeholder="Search" value />
+				<!-- ends a serch -->
+
+
+				<!-- starts insta icons -->
+				<div class="group_02">
+					<img class="icons" src="images/sns_icons/compass.svg" />
+					<img class="icons" src="images/sns_icons/valentines-heart.svg" />
+					<img class="icons" src="images/sns_icons/person.svg" />
+				</div>
+				<!-- ends insta icons -->
+
+			</div>
+		</header>
+	</nav>
+	<!-- ends a navigation -->
+
+
+	<!-- starts after login pages -->
+	<div class="wrap"> 
+		<main class="contentWrapper">
+			<section>
+
+				<!-- starts an insta -->
+				<div class="picBox">
+
+
+					<!-- starts a profile -->
+					<div class="profile">
+						<img class="proPic" src="images/profile.jpg" />
+						<span class="name"> cka720 </span>
+					</div>
+					<!-- ends a profile -->
+
+
+					<!-- starts a photo -->
+					<img class="photo" src="images/cat.png" />
+					<!-- ends a photo -->
+
+
+					<!-- starts a comment box -->
+					<div class="comtBox">
+						<div class="comtIcons">
+							<img class="comt_Icons redHeart" src="images/sns_icons/red-heart.svg" />
+							<img class="comt_Icons comtBubble" src="images/sns_icons/comt-bubble.svg" />
+						</div>
+						<div class="textBox">
+							<h1 class="likes">1,283 likes</h1>
+							<p class="text_01">
+								<strong>cka720</strong> 얘는 진짜 클수록 멋있음... <a class="more"> more </a>
+							</p>
+							<div class="text_02">Load more comments</div> 
+							<div class="text_03">
+								<p class="nickName"> <strong>haru_bro</strong> 왠지 고급진 시나모니~~~ </p> 
+								<p class="nickName"> <strong>tkgood2014</strong> 그래봐야 우리 생강이못따라와요! </p> 
+								<p class="nickName"> <strong>peniel0708</strong> 귀티가...... 와......... </p> 
+								<p class="nickName"> <strong>jhsoonie</strong> 테리우스 느낌~ </p> 
+								<div class="small"> 8 HOURS AGO </div>
+							</div> 
+						</div>
+					</div>
+					<!-- ends a comment box -->
+
+
+					<!-- starts a commect textarea -->
+					<form class="textarea">
+						<input class="textArea" type="text" placeholder="Add a comment..." value></input>
+						<img class="ellipsis" src="images/sns_icons/ellipsis.svg">
+					</form>
+					<!-- ends a commect textarea -->
+
+
+				</div>
+				<!-- ends an insta -->
+
+				<!-- starts an insta -->
+				<div class="picBox">
+
+
+					<!-- starts a profile -->
+					<div class="profile">
+						<img class="proPic" src="images/profile.jpg" />
+						<span class="name"> cka720 </span>
+					</div>
+					<!-- ends a profile -->
+
+
+					<!-- starts a photo -->
+					<img class="photo" src="images/cat.png" />
+					<!-- ends a photo -->
+
+
+					<!-- starts a comment box -->
+					<div class="comtBox">
+						<div class="comtIcons">
+							<img class="comt_Icons redHeart" src="images/sns_icons/red-heart.svg" />
+							<img class="comt_Icons comtBubble" src="images/sns_icons/comt-bubble.svg" />
+						</div>
+						<div class="textBox">
+							<h1 class="likes">1,283 likes</h1>
+							<p class="text_01">
+								<strong>cka720</strong> 얘는 진짜 클수록 멋있음... <a class="more"> more </a>
+							</p>
+							<div class="text_02">Load more comments</div> 
+							<div class="text_03">
+								<p class="nickName"> <strong>haru_bro</strong> 왠지 고급진 시나모니~~~ </p> 
+								<p class="nickName"> <strong>tkgood2014</strong> 그래봐야 우리 생강이못따라와요! </p> 
+								<p class="nickName"> <strong>peniel0708</strong> 귀티가...... 와......... </p> 
+								<p class="nickName"> <strong>jhsoonie</strong> 테리우스 느낌~ </p> 
+								<div class="small"> 8 HOURS AGO </div>
+							</div> 
+						</div>
+					</div>
+					<!-- ends a comment box -->
+
+
+					<!-- starts a commect textarea -->
+					<form class="textarea">
+						<input class="textArea" type="text" placeholder="Add a comment..." value></input>
+						<img class="ellipsis" src="images/sns_icons/ellipsis.svg">
+					</form>
+					<!-- ends a commect textarea -->
+
+
+				</div>
+				<!-- ends an insta -->
+
+			</section>
+		</main>
+	</div>
+	<!-- ends after login pages-->
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title> Instagram </title>
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+	<link rel="stylesheet" href="css/01_login.css">
+	<link rel="stylesheet" href="css/00_overlrap.css">
+
+	<style>
+		body{
+			overflow: hidden;
+		}
+		main{
+			width: 1200px;
+			height: 90%;
+		}
+	</style>
+
+</head>
+
+<body>
+
+	<!-- starts a sign-up page -->
+	<div class="wrap"> 
+		<main class="contentWrapper">
+
+
+			<!-- starts a sign-up -->
+			<section>
+				<img id="iphones" src="images/iphone.png">
+				<aside>
+					<img id="logo" src="images/sns_icons/instagram.svg">
+
+
+					<!--  starts a small info & with facebook -->
+					<div class="section_01">
+						<h2 id="h2_01"> Sign up to see photos and videos from your friends.</h2>
+						<button>
+							<img id="facebook" src="images/sns_icons/facebook.svg">
+							<span class="face_btn"> Log in with Facebook </span>
+						</button>
+					</div>
+					<!--  ends a small info & with facebook --> 
+
+
+					<!--  starts to separate -->
+					<div class="section_02">
+						<h4 class="line_01"></h3>
+						<span id="spanOr"> or </span>
+						<h4 class="line_02"></h3>
+					</div>
+					<!--  ends to separate -->
+
+
+					<!-- starts to register -->
+					<div class="section_03">
+						<input class="input" type="text" placeholder="Mobile Number or Email" value />
+						<input class="input" type="text" placeholder="Full Name" value />
+						<input class="input" type="text" placeholder="User Name" value />
+						<input class="input" type="text" placeholder="Password" value />
+					</div>
+					<!-- ends to register -->
+
+
+					<!-- starts a signup button -->
+					<div class="section_04">
+						<button>
+							<span class="sign_btn"> Sign Up </span>
+						</button>
+						<h2 id="h2_02"> By signing up, you agree to our</h2>
+						<p class="terms_privacy">
+							<a class="tp_link" href="#"> Terms </a> &amp; <a class="tp_link" href="#"> Privacy Policy. </a>
+						</p>
+					</div>
+					<!-- ends a signup button -->
+
+
+				</aside>
+
+
+				<!-- starts an account -->
+				<div class="account_box">
+					<p class="account"> Have an acount <a class="login_link" href="#"> Log in </a> </p>
+				</div>
+				<!-- ends an account -->
+
+
+				<!-- starts appshops -->
+				<div class="appshops">
+					<p class="app">Get the app.</p>
+					<div class="img_box">
+						<a href="https://itunes.apple.com/app/instagram/id389801252?mt=8"> <img class="app_img" src="images/appstore.png" /> </a>
+						<a id="googleplay" href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb%26utm_campaign%3DsignupPage%26utm_medium%3Dbadge"> <img class="app_img" src="images/googleplay.png" /> </a>
+					</div>
+				</div>
+				<!-- ends appshops -->
+
+
+			</section>
+			<!-- ends a sign-up -->
+
+
+		</main>
+
+		<footer>
+		<p class="footer_link">
+			<a class="foot_nav" href="https://www.instagram.com/about/us/"> ABOUT US </a>
+			<a class="foot_nav" href="https://help.instagram.com/"> SUPPORT </a>
+			<a class="foot_nav" href="http://blog.instagram.com/"> BLOG </a>
+			<a class="foot_nav" href="https://instagram-press.com/"> PRESS </a>
+			<a class="foot_nav" href="https://www.instagram.com/developer/"> API </a>
+			<a class="foot_nav" href="https://www.instagram.com/about/jobs/"> JOBS </a>
+			<a class="foot_nav" href="https://help.instagram.com/155833707900388"> PRIVACY </a>
+			<a class="foot_nav" href="https://help.instagram.com/478745558852511"> TERMS </a>
+			<a class="foot_nav" href="https://www.instagram.com/explore/locations/"> DIRETORY </a>
+			<a class="foot_nav" href=""> LANGUAGE </a>
+			<span class="copyright"> &copy; 2017 INSTAGRAM </span>
+		</p>
+	</footer>
+	</div>
+	<!-- ends a sign-up page -->
+
+</body>
+
+</html>
